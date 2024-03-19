8757ed4100179a9ed774225c00594799f02e753c
anvar2726 <xidiraliyevanvar@gmail.com>
2024-03-20 03:33:40 +0500

    added sound effect

--- js/app.js
+++ js/app.js
@@ -1,25 +1,35 @@
 const display = document.querySelector('.display');
 const buttons = document.querySelectorAll('.btn');
+const soundEffect = document.querySelector('.audio');
+const errorSound = document.querySelector('.error-sound');
+
 
 buttons.forEach(btn =>{
     btn.addEventListener('click', function(e){
         switch(e.target.innerText){
             case 'C':
+                soundEffect.play();
                 display.innerText = '';
                 break;
             case '←':
                if(display.innerText){
-                    display.innerText = display.innerText.slice(0, -1)
+                   soundEffect.play();
+                    display.innerText = display.innerText.slice(0, -1);
                }
                 break;
             case '=':
                 try{
+                    soundEffect.play();
                     display.innerText = eval(display.innerText);
                 }catch{
-                  display.innerText = 'Error!'  
+                    soundEffect.pause();
+                    errorSound.play();
+                    display.innerText = 'Error!' 
                 }
                 break;
-            default : display.innerText += e.target.innerText 
+            default : soundEffect.play();
+            display.innerText += e.target.innerText ;
+            
         }
     })
 })
